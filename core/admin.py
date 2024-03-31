3ca9a1f957edefe2fa1e402c035920c5e31baf76
ThiagoScrt <thiagolltt@gmail.com>
2024-03-31 10:08:28 -0300

    modified

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EveventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuarios', 'data_evento',)
+
+admin.site.register(Evento, EveventoAdmin)
