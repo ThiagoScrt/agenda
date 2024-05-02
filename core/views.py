bec9e5a51dee16267c35d14e5ccd563774e45e43
ThiagoScrt <thiagolltt@gmail.com>
2024-05-01 23:51:22 -0300

    create a html templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    usuarios = request.user
+    evento = Evento.objects.filter(usuarios=usuarios)
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
